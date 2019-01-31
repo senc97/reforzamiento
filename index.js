eeb99424136f00d84ac5454f507722bfc9b0c874
Giorgio <jlqs97@gmail.com>
2019-01-31 18:59:43 -0500

    Se cambió el index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require("path");
 
 app.set("port", process.env.PORT || 5000);
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/"));
 
 app.get("/", function(request, response) {
   response.sendFile(path.join(__dirname + "/index.html"));
